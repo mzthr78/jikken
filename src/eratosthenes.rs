e97205c288ae1a434cd1ddc241ef9eb09353049d
Mizuta Hiro <mzthr78@gmail.com>
2023-06-06 19:22:38 +0900

    add eratosthenes

---
+++ src/eratosthenes.rs
@@ -0,0 +1,25 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input!{
+        q: usize,
+        x: [usize; q],
+    }
+
+    let max = 300000; 
+
+    let mut is_prime = vec![true; max+1];
+    is_prime[0] = false;
+    is_prime[1] = false;
+
+    for i in 2..=max {
+        if !is_prime[i] { continue; }
+
+        println!("{}", i);
+
+        for j in ((i*2)..=max).step_by(i) {
+            is_prime[j] = false;   
+        }
+    }
+}
