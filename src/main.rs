9ae23aee51f5808cadf344ad98162f75df0de65c
Mizuta Hiro <mzthr78@gmail.com>
2023-09-06 11:04:42 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,79 @@
 use proconio::{fastout, input};
+use std::cmp::max;
+
+struct SegT {
+    s: usize,
+    v: Vec<usize>,
+}
+
+impl SegT {
+    fn new(n: usize) -> SegT {
+        let mut tmp = 1;
+
+        while tmp < n {
+            tmp *= 2;
+        }
+
+        SegT {
+            s: tmp,
+            v: vec![0; tmp*2],
+        }
+    }
+
+    fn update(&mut self, mut pos: usize, x: usize) {
+        pos = pos + self.s - 1;
+
+        self.v[pos] = x;
+
+        while pos > 1 {
+            pos /= 2;
+
+            self.v[pos] = max(self.v[pos*2], self.v[pos*2+1]);
+        }
+    }
+
+    fn query(&self, l: usize, r: usize, s: usize, e: usize, i: usize) -> isize {
+        if r <= s || e <= l {
+            return -1;
+        } else if l <= s && e <= r {
+            return self.v[i] as isize;
+        } else {
+            let m = (s + e) / 2;
+            let ansl = self.query(l, r, s, m, i * 2);
+            let ansr = self.query(l, r, m, e, i * 2 + 1);
+            return max(ansl, ansr);
+        }
+    }
+}
 
 #[fastout]
 fn main() {
     input!{
         n: usize,
-        k: usize,
-        mut a: [usize; n],
+        m: usize,
     }
 
-    a.insert(0, 0);
+    let mut s = SegT::new(n);
 
-    let mut p = vec![0; n];
-    p[0] = 1;
+    for _ in 0..m {
+        input!{
+            q: usize,
+        }
 
-    for i in 1..n {
-        p[i] = p[i-1];
-
-        for j in i+1..=n {
-            //if a[j] - a[i] > k { break; }
-            if a[j] > k + a[i] { break; }
-            p[i] = j;
+        if q == 1 {
+            input!{
+                pos: usize,
+                x: usize,
+            }
+            s.update(pos, x);
+        } else if q == 2 {
+            input!{
+                l: usize,
+                r: usize,
+            }
+            //let ans = s.query(l, r, 1, s.s, 1);
+            let ans = s.query(l, r, 1, s.s + 1, 1);
+            println!("{}", ans);
         }
     }
-
-    println!("{:?}", p);
-
-    let mut ans = 0;
-    for i in 1..n {
-        ans += p[i] - i;
-    }
-    println!("{}", ans);
 }
-
-/*
-in
-7 10
-11 12 16 22 27 28 31
-
-out
-11
-*/
-
